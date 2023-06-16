e6e5c27cbf936ca5cf2ef8deb20e2196d55c762d
Neat <sophaneat@student.fdu.edu>
2023-06-16 12:13:52 -0700

    Add login page and fix header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import "./Header.scss";
 import logo from "../../Assets/Icons/apex-hero-medium-logo-light 1.png";
 import chevrondown from "../../Assets/Icons/cheverondown.png";
+import { Link } from 'react-router-dom';
+import mentorLogo from "../../Assets/Icons/Apex Mentor Connect Logo White (2).png";
 const Header = () => {
     return (
         <div className="header">
             <div className="header__col">
-                <img src={logo} alt="" className="header__logo" />
+                <Link to="/" className="header__title-link">
+                    <div className="header__logo-wrapper">
+                        <img src={logo} alt="" className="header__logo" />
+                        <p className="header__title2">x</p>
+                        <img src={mentorLogo} alt="" className="header__logo2" />
+                    </div>
+                </Link>
                 <div className="header__nav">
                     <div className="header__nav__link">
                         <p className="header__link">ABOUT</p>
--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
-
+import React from 'react';
+import "./LoginPage.scss";
+import eaLogo from "../../Assets/Icons/Electronic-Arts-Logo.svg.png";
+import avatar from "../../Assets/Icons/cea82eb3-a532-4fe5-9d0a-f978116c8386.avif";
+import checkIcon from "../../Assets/Icons/login.png";
+import { Link } from 'react-router-dom';
 const LoginPage = () => {
     return (
-        <div>LoginPage</div>
+        <div className="login">
+            <img src={eaLogo} alt="" className="login__logo" />
+            <h2 className="login__title">Quick Login</h2>
+            <div className="login__username">
+                <img src={avatar} className="login__avatar" alt=""/>
+                <div className="login__para">
+                    <p className="login__name">Username: Mind_Hunter</p>
+                    <p className="login__id">User id: 88484gh23g434hh</p>
+                </div>
+                <Link to="/preferences">
+                    <img src={checkIcon} alt="" className="login__check" />
+                </Link>
+            </div>
+            <div className="login__form">
+                <h1 className="login__title">Login</h1>
+                <div className="login__form-control">
+                    <input placeholder='Username' type="text" className="login__input" />
+                    <input placeholder='Password' type="text" className="login__input" />
+                    <Link to="/preferences">
+                        <button className="login__btn">Login</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
