781759ccf5af6628c2c9e66eabcfff9ce6c1bf2a
#Mae <mae@hashedmae.gay>
2023-06-16 14:35:07 -0700

    ast

--- src/Components/MenteeForm/MenteeForm.jsx
+++ src/Components/MenteeForm/MenteeForm.jsx
@@ -16,124 +16,38 @@ import casualIcon from "../../Assets/Icons/casual.svg";
 import ladderIcon from "../../Assets/Icons/ladder.svg";
 import generalIcon from "../../Assets/Icons/crosshair.png";
 
-const MenteeForm = ({
-  filterMode,
-  setFilterMode,
-  filterSkill,
-  setFilterSkill,
-  filterCharacter,
-  setFilterCharacter
-}) => {
+const MenteeForm = ({ filterMode, setFilterMode, filterSkill, setFilterSkill, filterCharacter, setFilterCharacter }) => {
+  
   const navigate = useNavigate();
 
-  console.log(setFilterSkill);
+  
 
   return (
     <div className="mentee-form">
       <h2 className="mentee-form__preference-title">Skill Level</h2>
       <section className="mentee-form__value-options">
-        <MenteeFormOption
-          selectValue={"bronze"}
-          filterValue={filterSkill}
-          setFilterValue={setFilterSkill}
-          imgSrc={bronzeIcon}
-          classSelect={"value-select"}
-        />
-        <MenteeFormOption
-          selectValue={"silver"}
-          filterValue={filterSkill}
-          setFilterValue={setFilterSkill}
-          imgSrc={silverIcon}
-          classSelect={"value-select"}
-        />
-        <MenteeFormOption
-          selectValue={"gold"}
-          filterValue={filterSkill}
-          setFilterValue={setFilterSkill}
-          imgSrc={goldIcon}
-          classSelect={"value-select"}
-        />
-        <MenteeFormOption
-          selectValue={"platinum+"}
-          filterValue={filterSkill}
-          setFilterValue={setFilterSkill}
-          imgSrc={platIcon}
-          classSelect={"value-select"}
-        />
+        <MenteeFormOption  selectValue={"Bronze"} filterValue={filterSkill} setFilterValue={setFilterSkill} imgSrc={bronzeIcon} classSelect={"value-select"} />
+        <MenteeFormOption selectValue={"Silver"} filterValue={filterSkill} setFilterValue={setFilterSkill} imgSrc={silverIcon} classSelect={"value-select"} />
+        <MenteeFormOption selectValue={"Gold"} filterValue={filterSkill} setFilterValue={setFilterSkill} imgSrc={goldIcon} classSelect={"value-select"} />
+        <MenteeFormOption selectValue={"Platinum+"} filterValue={filterSkill} setFilterValue={setFilterSkill} imgSrc={platIcon} classSelect={"value-select"} />
       </section>
       <h2 className="mentee-form__preference-title">Game Mode</h2>
       <section className="mentee-form__value-options">
-        <MenteeFormOption
-          selectValue={"public games"}
-          filterValue={filterMode}
-          setFilterValue={setFilterMode}
-          imgSrc={casualIcon}
-          classSelect={"value-select"}
-        />
-        <MenteeFormOption
-          selectValue={"ranked"}
-          filterValue={filterMode}
-          setFilterValue={setFilterMode}
-          imgSrc={ladderIcon}
-          classSelect={"value-select"}
-        />
-        <MenteeFormOption
-          selectValue={"general gameplay"}
-          filterValue={filterMode}
-          setFilterValue={setFilterMode}
-          imgSrc={generalIcon}
-          classSelect={"value-select"}
-        />
+        <MenteeFormOption selectValue={"Pubs"} filterValue={filterMode} setFilterValue={setFilterMode} imgSrc={casualIcon} classSelect={"value-select"}  />
+        <MenteeFormOption selectValue={"Ranked"} filterValue={filterMode} setFilterValue={setFilterMode} imgSrc={ladderIcon} classSelect={"value-select"} />
+        <MenteeFormOption selectValue={"General Gameplay"} filterValue={filterMode} setFilterValue={setFilterMode} imgSrc={generalIcon} classSelect={"value-select"} />
       </section>
       <h2 className="mentee-form__preference-title">Character Role</h2>
       <section className="mentee-form__character-options">
-        <MenteeFormOption
-          selectValue={"assault"}
-          filterValue={filterCharacter}
-          setFilterValue={setFilterCharacter}
-          imgSrc={assaultIcon}
-          classSelect={"character-select"}
-        />
-        <MenteeFormOption
-          selectValue={"recon"}
-          filterValue={filterCharacter}
-          setFilterValue={setFilterCharacter}
-          imgSrc={reconIcon}
-          classSelect={"character-select"}
-        />
-        <MenteeFormOption
-          selectValue={"skirmisher"}
-          filterValue={filterCharacter}
-          setFilterValue={setFilterCharacter}
-          imgSrc={skirmisherIcon}
-          classSelect={"character-select"}
-        />
-        <MenteeFormOption
-          selectValue={"controller"}
-          filterValue={filterCharacter}
-          setFilterValue={setFilterCharacter}
-          imgSrc={controllerIcon}
-          classSelect={"character-select"}
-        />
-        <MenteeFormOption
-          selectValue={"support"}
-          filterValue={filterCharacter}
-          setFilterValue={setFilterCharacter}
-          imgSrc={supportIcon}
-          classSelect={"character-select"}
-        />
+      <MenteeFormOption selectValue={"Assault"} filterValue={filterCharacter} setFilterValue={setFilterCharacter} imgSrc={assaultIcon} classSelect={"character-select"} />
+      <MenteeFormOption selectValue={"Recon"} filterValue={filterCharacter} setFilterValue={setFilterCharacter} imgSrc={reconIcon} classSelect={"character-select"} />
+      <MenteeFormOption selectValue={"Skirmisher"} filterValue={filterCharacter} setFilterValue={setFilterCharacter} imgSrc={skirmisherIcon} classSelect={"character-select"} />
+      <MenteeFormOption selectValue={"Controller"} filterValue={filterCharacter} setFilterValue={setFilterCharacter} imgSrc={controllerIcon} classSelect={"character-select"} />
+      <MenteeFormOption selectValue={"Support"} filterValue={filterCharacter} setFilterValue={setFilterCharacter} imgSrc={supportIcon} classSelect={"character-select"} />
       </section>
       <div className="mentee-form__button-wrapper">
-        <button
-          className="mentee-form__submit"
-          to="/dashboard"
-          onClick={() => {
-            navigate("/dashboard");
-          }}
-        >
-          NEXT
-        </button>
-      </div>
+      <button className="mentee-form__submit" to="/dashboard" onClick={()=>{navigate("/dashboard")}}>NEXT</button></div>
+
     </div>
   );
 };
