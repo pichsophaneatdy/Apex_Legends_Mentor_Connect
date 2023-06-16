3e11b73bb65e2d069eea3d4d6033f7b159d0875f
Neat <sophaneat@student.fdu.edu>
2023-06-16 09:23:52 -0700

    Add react routes and pages

---
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FilterNav from '../../Components/FilterNav/FilterNav'
+
+const Dashboard = ({handleSkillClick, handleCharacterClick, handleModeClick}) => {
+    return (
+        <div>
+            Dashboard
+            <FilterNav />
+
+        </div>
+    )
+}
+
+export default Dashboard
