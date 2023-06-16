fd0dc59e5fc4bbc1a3c1b50a5630c1f122f88991
Neat <sophaneat@student.fdu.edu>
2023-06-16 13:21:08 -0700

    Add card component

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 import FilterNav from "../../Components/FilterNav/FilterNav";
-
+import "./Dashboard.scss";
+import Card from "../../Components/Card Component/Card";
 const Dashboard = ({
-  handleSkillClick,
-  handleCharacterClick,
-  handleModeClick,
-  filterMode,
-  filterSkill,
-  filterCharacter,
+    handleSkillClick,
+    handleCharacterClick,
+    handleModeClick,
+    filterMode,
+    filterSkill,
+    filterCharacter,
 }) => {
-  return (
-    <div>
-      Dashboard
-      <FilterNav
-        handleSkillClick={handleSkillClick}
-        handleCharacterClick={handleCharacterClick}
-        handleModeClick={handleModeClick}
-        filterMode={filterMode}
-        filterSkill={filterSkill}
-        filterCharacter={filterCharacter}
-      />
-    </div>
-  );
+    return (
+        <div className="dashboard">
+            <FilterNav
+                handleSkillClick={handleSkillClick}
+                handleCharacterClick={handleCharacterClick}
+                handleModeClick={handleModeClick}
+                filterMode={filterMode}
+                filterSkill={filterSkill}
+                filterCharacter={filterCharacter}
+            />
+            <div className="cardList">
+                <Card />
+            </div>
+        </div>
+    );
 };
 
 export default Dashboard;
